8e1ede1c2a2020d4a96b208fc6a03273863e4342
dagxam <dagxam@gmail.com>
2026-09-17 13:56:30 +0300

    Update ShipGUIListener.java

--- src/main/java/com/dagxam/moveship/listeners/ShipGUIListener.java
+++ src/main/java/com/dagxam/moveship/listeners/ShipGUIListener.java
@@ -1,49 +1,49 @@
 package com.dagxam.moveship.listeners;
 
+import com.dagxam.moveship.core.ShipScanner;
 import com.dagxam.moveship.gui.ShipGUI;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
+import java.util.Set;
+
 public class ShipGUIListener implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        // Проверяем, является ли инвентарь нашим интерфейсом
         if (event.getInventory().getHolder() instanceof ShipGUI gui) {
             
-            // Обязательно отменяем клик, чтобы игрок не забрал кнопки себе
             event.setCancelled(true);
 
             if (!(event.getWhoClicked() instanceof Player player)) return;
-            
-            // Защита от кликов по нижнему инвентарю (своему инвентарю игрока)
             if (event.getClickedInventory() == null || !event.getClickedInventory().equals(event.getInventory())) return;
 
-            // Получаем координаты кафедры, к которой привязано это меню
             Location coreLoc = gui.getCoreLocation();
 
-            // Проверяем, по какому слоту кликнули
             switch (event.getSlot()) {
                 case 0: // Сканировать
                     player.sendMessage(Component.text("Запуск сканирования...", NamedTextColor.AQUA));
-                    // TODO: Здесь мы вызовем алгоритм Flood Fill
+                    
+                    // Вызываем наш алгоритм сканирования
+                    Set<Block> shipBlocks = ShipScanner.scanShip(coreLoc);
+                    
+                    player.sendMessage(Component.text("Корабль отсканирован! Найдено блоков: " + shipBlocks.size(), NamedTextColor.GREEN));
                     player.closeInventory();
                     break;
                     
                 case 3: // Активировать
                     player.sendMessage(Component.text("Корабль активирован!", NamedTextColor.GREEN));
-                    // TODO: Здесь мы превратим блоки в сущности и посадим игрока
                     player.closeInventory();
                     break;
                     
                 case 5: // Остановить
                     player.sendMessage(Component.text("Корабль остановлен.", NamedTextColor.RED));
-                    // TODO: Возврат сущностей обратно в твердые блоки
                     player.closeInventory();
                     break;
                     
