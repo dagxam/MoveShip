29a3dbd96d771128346c2506d83499ab2a44d568
dagxam <dagxam@gmail.com>
2026-09-20 13:58:31 +0300

    Update ShipBlockData.java

--- src/main/java/com/dagxam/moveship/core/ShipBlockData.java
+++ src/main/java/com/dagxam/moveship/core/ShipBlockData.java
@@ -11,30 +11,16 @@ public class ShipBlockData {
     private final BlockState stateSnapshot;
     private final ItemStack[] items;
 
-    public ShipBlockData(Vector relativeOffset, BlockData blockData, BlockState stateSnapshot, ItemStack[] items) {
+    public ShipBlockData(Vector relativeOffset, BlockData blockData,
+                         BlockState stateSnapshot, ItemStack[] items) {
         this.relativeOffset = relativeOffset;
         this.blockData = blockData;
         this.stateSnapshot = stateSnapshot;
         this.items = items;
     }
 
-    public ShipBlockData(Vector relativeOffset, BlockData blockData, BlockState stateSnapshot) {
-        this(relativeOffset, blockData, stateSnapshot, null);
-    }
-
-    public Vector getRelativeOffset() {
-        return relativeOffset;
-    }
-
-    public BlockData getBlockData() {
-        return blockData;
-    }
-
-    public BlockState getStateSnapshot() {
-        return stateSnapshot;
-    }
-
-    public ItemStack[] getItems() {
-        return items;
-    }
+    public Vector getRelativeOffset() { return relativeOffset; }
+    public BlockData getBlockData()   { return blockData; }
+    public BlockState getStateSnapshot() { return stateSnapshot; }
+    public ItemStack[] getItems()     { return items; }
 }
