9e50d07f3523bbdc27188bf691ae6f63aae4d0a1
dagxam <dagxam@gmail.com>
2026-09-20 11:26:21 +0300

    Update ShipBlockData.java

--- src/main/java/com/dagxam/moveship/core/ShipBlockData.java
+++ src/main/java/com/dagxam/moveship/core/ShipBlockData.java
@@ -2,17 +2,24 @@ package com.dagxam.moveship.core;
 
 import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 public class ShipBlockData {
     private final Vector relativeOffset;
     private final BlockData blockData;
     private final BlockState stateSnapshot;
+    private final ItemStack[] items;
 
-    public ShipBlockData(Vector relativeOffset, BlockData blockData, BlockState stateSnapshot) {
+    public ShipBlockData(Vector relativeOffset, BlockData blockData, BlockState stateSnapshot, ItemStack[] items) {
         this.relativeOffset = relativeOffset;
         this.blockData = blockData;
         this.stateSnapshot = stateSnapshot;
+        this.items = items;
+    }
+
+    public ShipBlockData(Vector relativeOffset, BlockData blockData, BlockState stateSnapshot) {
+        this(relativeOffset, blockData, stateSnapshot, null);
     }
 
     public Vector getRelativeOffset() {
@@ -25,5 +32,9 @@ public class ShipBlockData {
 
     public BlockState getStateSnapshot() {
         return stateSnapshot;
+    }
+
+    public ItemStack[] getItems() {
+        return items;
     }
 }
