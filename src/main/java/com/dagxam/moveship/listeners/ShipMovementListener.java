629dee46c989d7e0b8f75e4ccbbd0e3b9e634a6d
dagxam <dagxam@gmail.com>
2026-09-20 13:39:30 +0300

    Update ShipMovementListener.java

--- src/main/java/com/dagxam/moveship/listeners/ShipMovementListener.java
+++ src/main/java/com/dagxam/moveship/listeners/ShipMovementListener.java
@@ -1,79 +1,40 @@
-package com.dagxam.moveship.listeners;
+package com.dagxam.moveship.core;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.ProtocolManager;
-import com.comphenix.protocol.events.ListenerPriority;
-import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketEvent;
-import com.dagxam.moveship.MoveShipPlugin;
-import com.dagxam.moveship.core.ActiveShip;
-import com.dagxam.moveship.core.ShipManager;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDismountEvent;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
-public class ShipMovementListener implements Listener {
+public class ShipBlockData {
+    private final Vector relativeOffset;
+    private final BlockData blockData;
+    private final BlockState stateSnapshot;
+    private final ItemStack[] items;
 
-    public ShipMovementListener(MoveShipPlugin plugin) {
-        Bukkit.getPluginManager().registerEvents(this, plugin);
-
-        ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
-        
-        protocolManager.addPacketListener(new PacketAdapter(plugin, ListenerPriority.NORMAL, PacketType.Play.Client.STEER_VEHICLE) {
-            @Override
-            public void onPacketReceiving(PacketEvent event) {
-                Player player = event.getPlayer();
-                ActiveShip ship = ShipManager.getShip(player);
-
-                if (ship == null) return;
-
-                boolean fwd = false, bwd = false, l = false, r = false;
-
-                try {
-                    // Парсинг нового формата пакета (1.20.6+ Record Input)
-                    Object inputObj = event.getPacket().getModifier().read(0);
-                    if (inputObj != null) {
-                        String inputStr = inputObj.toString();
-                        fwd = inputStr.contains("forward=true");
-                        bwd = inputStr.contains("backward=true");
-                        l = inputStr.contains("left=true");
-                        r = inputStr.contains("right=true");
-                    }
-                } catch (Exception ignored) {
-                    // Старый формат (до 1.20.4)
-                    float sideVal = event.getPacket().getFloat().readSafely(0);
-                    float forwardVal = event.getPacket().getFloat().readSafely(1);
-                    fwd = forwardVal > 0;
-                    bwd = forwardVal < 0;
-                    l = sideVal > 0;
-                    r = sideVal < 0;
-                }
-
-                final boolean finalFwd = fwd;
-                final boolean finalBwd = bwd;
-                final boolean finalL = l;
-                final boolean finalR = r;
-
-                // Немедленно передаем состояние кнопок в ядро корабля
-                Bukkit.getScheduler().runTask(plugin, () -> {
-                    ship.setInput(finalFwd, finalBwd, finalL, finalR);
-                });
-            }
-        });
+    public ShipBlockData(Vector relativeOffset, BlockData blockData, BlockState stateSnapshot, ItemStack[] items) {
+        this.relativeOffset = relativeOffset;
+        this.blockData = blockData;
+        this.stateSnapshot = stateSnapshot;
+        this.items = items;
     }
 
-    @EventHandler
-    public void onDismount(EntityDismountEvent event) {
-        if (!(event.getEntity() instanceof Player player)) return;
+    public ShipBlockData(Vector relativeOffset, BlockData blockData, BlockState stateSnapshot) {
+        this(relativeOffset, blockData, stateSnapshot, null);
+    }
 
-        if (ShipManager.getShip(player) != null) {
-            ShipManager.stopShip(player);
-            player.sendMessage(Component.text("Вы покинули штурвал. Корабль зафиксирован.", NamedTextColor.YELLOW));
-        }
+    public Vector getRelativeOffset() {
+        return relativeOffset;
+    }
+
+    public BlockData getBlockData() {
+        return blockData;
+    }
+
+    public BlockState getStateSnapshot() {
+        return stateSnapshot;
+    }
+
+    public ItemStack[] getItems() {
+        return items;
     }
 }
