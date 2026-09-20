1040d819a30de9b2b988c0e7c1fcd8eee6ce25e4
dagxam <dagxam@gmail.com>
2026-09-20 14:53:31 +0300

    Update MoveShipPlugin.java

--- src/main/java/com/dagxam/moveship/MoveShipPlugin.java
+++ src/main/java/com/dagxam/moveship/MoveShipPlugin.java
@@ -1,30 +1,61 @@
-Также убери из MoveShipPlugin.java создание ShipMovementListener через ProtocolLib — теперь он не нужен, просто регистрируй как обычный listener:
+package com.dagxam.moveship;
 
-Java
+import com.dagxam.moveship.core.ShipManager;
+import com.dagxam.moveship.listeners.ShipControllerListener;
+import com.dagxam.moveship.listeners.ShipGUIListener;
+import com.dagxam.moveship.listeners.ShipMovementListener;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Tag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
 
-// В onEnable() замени:
-// new ShipMovementListener(this);  // УБЕРИ ЭТО
+public class MoveShipPlugin extends JavaPlugin {
 
-// На:
-getServer().getPluginManager().registerEvents(new ShipMovementListener(this), this);
-И убери из pom.xml зависимость ProtocolLib если больше не используется нигде:
+    public static NamespacedKey CONTROLLER_KEY;
 
-XML
+    @Override
+    public void onEnable() {
+        CONTROLLER_KEY = new NamespacedKey(this, "ship_controller");
 
-<!-- Если ProtocolLib больше не нужен — убери эту зависимость -->
-<dependency>
-    <groupId>com.comphenix.protocol</groupId>
-    <artifactId>ProtocolLib</artifactId>
-    <version>5.3.0</version>
-    <scope>provided</scope>
-</dependency>
-И из plugin.yml:
+        registerShipControllerRecipe();
 
-YAML
+        getServer().getPluginManager().registerEvents(new ShipControllerListener(), this);
+        getServer().getPluginManager().registerEvents(new ShipGUIListener(), this);
+        getServer().getPluginManager().registerEvents(new ShipMovementListener(this), this);
 
-# depend убери если ProtocolLib не нужен
-depend: []
+        getLogger().info("MoveShip запущен!");
+    }
 
+    @Override
+    public void onDisable() {
+        ShipManager.stopAllShips();
+        getLogger().info("MoveShip отключен.");
+    }
 
+    private void registerShipControllerRecipe() {
+        ItemStack controllerItem = new ItemStack(Material.LECTERN);
+        ItemMeta meta = controllerItem.getItemMeta();
+        if (meta != null) {
+            meta.displayName(Component.text("Кафедра управления", NamedTextColor.GOLD));
+            meta.getPersistentDataContainer().set(CONTROLLER_KEY, PersistentDataType.BYTE, (byte) 1);
+            controllerItem.setItemMeta(meta);
+        }
 
+        NamespacedKey recipeKey = new NamespacedKey(this, "ship_controller_recipe");
+        Bukkit.removeRecipe(recipeKey);
 
+        ShapedRecipe recipe = new ShapedRecipe(recipeKey, controllerItem);
+        recipe.shape("PPP", "LLL");
+        recipe.setIngredient('P', new RecipeChoice.MaterialChoice(Tag.PLANKS));
+        recipe.setIngredient('L', new RecipeChoice.MaterialChoice(Tag.LOGS));
+        Bukkit.addRecipe(recipe);
+    }
+}
