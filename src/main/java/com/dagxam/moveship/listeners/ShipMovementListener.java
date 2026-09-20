30609d194044d26c69e350be872fb8f8d66617a1
dagxam <dagxam@gmail.com>
2026-09-20 14:53:46 +0300

    Update ShipMovementListener.java

--- src/main/java/com/dagxam/moveship/listeners/ShipMovementListener.java
+++ src/main/java/com/dagxam/moveship/listeners/ShipMovementListener.java
@@ -17,60 +17,6 @@ import org.bukkit.input.PlayerInput;
 public class ShipMovementListener implements Listener {
 
     public ShipMovementListener(MoveShipPlugin plugin) {
-        // регистрация происходит в MoveShipPlugin через registerEvents
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerInput(PlayerInputEvent event) {
-        Player player = event.getPlayer();
-        ActiveShip ship = ShipManager.getShip(player);
-        if (ship == null) return;
-
-        PlayerInput input = event.getInput();
-
-        ship.setInput(
-            input.forward(),
-            input.backward(),
-            input.left(),
-            input.right()
-        );
-    }
-
-    @EventHandler
-    public void onDismount(EntityDismountEvent event) {
-        if (!(event.getEntity() instanceof Player player)) return;
-        if (ShipManager.getShip(player) != null) {
-            ShipManager.stopShip(player);
-            player.sendMessage(Component.text(
-                "Вы покинули штурвал. Корабль зафиксирован.",
-                NamedTextColor.YELLOW));
-        }
-    }
-
-    @EventHandler
-    public void onQuit(PlayerQuitEvent event) {
-        ShipManager.stopShip(event.getPlayer());
-    }
-}package com.dagxam.moveship.listeners;
-
-import com.dagxam.moveship.MoveShipPlugin;
-import com.dagxam.moveship.core.ActiveShip;
-import com.dagxam.moveship.core.ShipManager;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDismountEvent;
-import org.bukkit.event.player.PlayerInputEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.input.PlayerInput;
-
-public class ShipMovementListener implements Listener {
-
-    public ShipMovementListener(MoveShipPlugin plugin) {
-        // регистрация происходит в MoveShipPlugin через registerEvents
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
