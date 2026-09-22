65391ae8ab10cf8b617377eb3706fd3d668f3083
dagxam <dagxam@gmail.com>
2026-09-22 11:44:22 +0300

    Fix MoveShip core and build

--- src/main/java/com/dagxam/moveship/core/ShipBlockData.java
+++ src/main/java/com/dagxam/moveship/core/ShipBlockData.java
@@ -2,25 +2,46 @@ package com.dagxam.moveship.core;
 
 import org.bukkit.block.BlockState;
 import org.bukkit.block.data.BlockData;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.util.Vector;
 
 public class ShipBlockData {
-    private final Vector relativeOffset;
+
+    private final int localX;
+    private final int localY;
+    private final int localZ;
     private final BlockData blockData;
     private final BlockState stateSnapshot;
-    private final ItemStack[] items;
 
-    public ShipBlockData(Vector relativeOffset, BlockData blockData,
-                         BlockState stateSnapshot, ItemStack[] items) {
-        this.relativeOffset = relativeOffset;
+    public ShipBlockData(
+            int localX,
+            int localY,
+            int localZ,
+            BlockData blockData,
+            BlockState stateSnapshot
+    ) {
+        this.localX = localX;
+        this.localY = localY;
+        this.localZ = localZ;
         this.blockData = blockData;
         this.stateSnapshot = stateSnapshot;
-        this.items = items;
     }
 
-    public Vector getRelativeOffset() { return relativeOffset; }
-    public BlockData getBlockData()   { return blockData; }
-    public BlockState getStateSnapshot() { return stateSnapshot; }
-    public ItemStack[] getItems()     { return items; }
+    public int getLocalX() {
+        return localX;
+    }
+
+    public int getLocalY() {
+        return localY;
+    }
+
+    public int getLocalZ() {
+        return localZ;
+    }
+
+    public BlockData getBlockData() {
+        return blockData;
+    }
+
+    public BlockState getStateSnapshot() {
+        return stateSnapshot;
+    }
 }
