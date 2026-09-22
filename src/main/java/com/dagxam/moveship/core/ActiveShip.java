380fee55750565d289e378d3378abe6eb65dfc1c
dagxam <dagxam@gmail.com>
2026-09-22 13:23:01 +0300

    Исправить импорт BlockFace в ActiveShip

--- src/main/java/com/dagxam/moveship/core/ActiveShip.java
+++ src/main/java/com/dagxam/moveship/core/ActiveShip.java
@@ -7,6 +7,7 @@ import org.bukkit.World;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.Container;
 import org.bukkit.block.data.BlockData;
