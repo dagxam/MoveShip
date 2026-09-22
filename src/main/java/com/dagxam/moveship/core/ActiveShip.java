5783e747de408b1d8819c59e4b2aaaec4613cff8
dagxam <dagxam@gmail.com>
2026-09-22 11:45:28 +0300

    Remove obsolete rotation table from ActiveShip

--- src/main/java/com/dagxam/moveship/core/ActiveShip.java
+++ src/main/java/com/dagxam/moveship/core/ActiveShip.java
@@ -93,25 +93,6 @@ public class ActiveShip {
     private final double seatLocalY;
     private final double seatLocalZ;
 
-    private static final List<BlockFace> ROT16 = List.of(
-            BlockFace.NORTH,
-            BlockFace.NORTH_NORTH_EAST,
-            BlockFace.NORTH_EAST,
-            BlockFace.EAST_NORTH_EAST,
-            BlockFace.EAST,
-            BlockFace.EAST_SOUTH_EAST,
-            BlockFace.SOUTH_EAST,
-            BlockFace.SOUTH_SOUTH_EAST,
-            BlockFace.SOUTH,
-            BlockFace.SOUTH_SOUTH_WEST,
-            BlockFace.SOUTH_WEST,
-            BlockFace.WEST_SOUTH_WEST,
-            BlockFace.WEST,
-            BlockFace.WEST_NORTH_WEST,
-            BlockFace.NORTH_WEST,
-            BlockFace.NORTH_NORTH_WEST
-    );
-
     public ActiveShip(Set<Block> blocks, Location anchorLocation, Player pilot) {
         if (blocks == null || blocks.isEmpty()) {
             throw new IllegalArgumentException("Корабль не содержит блоков");
