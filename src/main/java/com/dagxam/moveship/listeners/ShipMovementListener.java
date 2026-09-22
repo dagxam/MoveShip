f7e702ea937a31d2d498b3623e803a56425c7f75
dagxam <dagxam@gmail.com>
2026-09-22 08:50:01 +0300

    Сделать текущее состояние управления единственным источником W/A/S/D

--- src/main/java/com/dagxam/moveship/listeners/ShipMovementListener.java
+++ src/main/java/com/dagxam/moveship/listeners/ShipMovementListener.java
@@ -1,50 +1,25 @@
 package com.dagxam.moveship.listeners;
 
 import com.dagxam.moveship.MoveShipPlugin;
-import com.dagxam.moveship.core.ActiveShip;
 import com.dagxam.moveship.core.ShipManager;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.Input;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDismountEvent;
-import org.bukkit.event.player.PlayerInputEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 /**
- * Управление активным кораблем.
+ * Обработчики жизненного цикла управления кораблем.
  *
- * Как и в проверенной архитектуре SimpleShips, управление W/A/S/D
- * поступает через нативный Paper PlayerInputEvent.
- *
- * Направление взгляда игрока здесь не перехватывается:
- * yaw/pitch принадлежат игроку, а yaw штурвала после активации заморожен.
+ * W/A/S/D не обрабатываются через событие изменения input.
+ * ActiveShip каждый тик получает актуальное состояние через
+ * Player#getCurrentInput(), что корректно сохраняет удержание клавиши.
  */
 public final class ShipMovementListener implements Listener {
 
     public ShipMovementListener(MoveShipPlugin plugin) {
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onPlayerInput(PlayerInputEvent event) {
-        Player player = event.getPlayer();
-        ActiveShip ship = ShipManager.getShip(player);
-
-        if (ship == null) {
-            return;
-        }
-
-        Input input = event.getInput();
-
-        ship.setInput(
-                input.isForward(),
-                input.isBackward(),
-                input.isLeft(),
-                input.isRight()
-        );
     }
 
     @EventHandler
